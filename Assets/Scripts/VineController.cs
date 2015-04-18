0c4af122d3794947f717ebf0f01e1031aec31812
Ben M <bmenesini@gmail.com>
2015-04-18 15:08:54 -0700

    Added vines

---
+++ Assets/Scripts/VineController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VineController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player Shot"){
+			Destroy (gameObject);
+		}
+	}
+
+}
