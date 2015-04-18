eabc23034e4c0943f28294198f0948bc0a5cad43
Ben M <bmenesini@gmail.com>
2015-04-18 12:49:49 -0700

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5;
+	public float maxEnergy = 100;
+	public float maxSpotAngle = 55.0f;
+	public float minSpotAngle = 15.0f;
+	public float maxIntensity = 4.0f;
+	public float minIntensity = 2.0f;
+	public float fireDelay = 0.5f;
+	public float fireCost = 5.0f;
+	public float rotateSpeed = 50.0f;
+
+	public GameObject playerShot;
+	public Transform shotSpawn;
+	public Light playerLight;
+	public Camera mainCamera;
+
+	private float nextFire;
+	private float energy;
+
+	void Start(){
+		energy = maxEnergy;
+	}
+
+	void Update(){
+		UpdateLighting ();
+
+		if(Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireDelay;
+			Instantiate(playerShot, shotSpawn.position, transform.rotation);
+			energy -= fireCost;
+		}
+	}
+
+	void FixedUpdate(){
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		Vector3	movement = new Vector3(moveX, 0.0f, moveY);
+
+		rigidbody.AddRelativeForce(movement * speed * Time.deltaTime);
+
+		transform.Rotate(Vector3.up, rotateSpeed * moveX * Time.deltaTime);
+	}
+
+	private void UpdateLighting(){
+		playerLight.intensity = Mathf.Clamp(
+			(energy / maxEnergy) * maxIntensity,
+			minIntensity,
+			maxIntensity
+			);
+		playerLight.spotAngle = Mathf.Clamp(
+			(energy / maxEnergy) * maxSpotAngle,
+			minSpotAngle,
+			maxSpotAngle
+			);
+	}
+}
