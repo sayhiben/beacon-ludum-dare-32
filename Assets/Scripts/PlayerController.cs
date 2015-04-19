496403fd2f53d1bbd2abcdb8be729187bee93fbb
Ben M <bmenesini@gmail.com>
2015-04-18 20:50:56 -0700

    added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,51 +13,45 @@ public class PlayerController : MonoBehaviour {
 	public float fireCost = 5.0f;
 	public float rotateSpeed = 50.0f;
 	public float jumpCost = 30.0f;
-	public float jumpPower = 20.0f;
+	public float jumpPower = 1500.0f;
+	public float jumpLightMultiplier = 1.5f;
 
 	public GameObject playerShot;
 	public Transform shotSpawn;
 	public Light playerLight;
-	public Camera mainCamera;
+	public Camera mainCamera; 
 
 	private float nextFire;
 	private float energy;
 	private bool canMove = true;
 	private bool isJumping = false;
+	private Rigidbody rigidbody;
+
 
 	void Start(){
 		energy = maxEnergy;
+		rigidbody = GetComponent<Rigidbody>();
 	}
 
 	void Update(){
 		UpdateLighting ();
+		HandleFire();
+		StayOnTheFloor();
+	}
 
-		if(transform.position.y <= 1){
-			if(isJumping){
-				isJumping = false;
-				canMove = true;
-			}
-			transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
-		}
-
-		if(Input.GetButton("Fire1") && Time.time > nextFire) {
+	void HandleFire(){
+		if(Input.GetButton("Fire1") && Time.time > nextFire && energy >= fireCost) {
 			nextFire = Time.time + fireDelay;
 			Instantiate(playerShot, shotSpawn.position, transform.rotation);
 			energy -= fireCost;
 		}
-
-		if(Input.GetKey(KeyCode.Space) && canMove){
-			canMove = false;
-			isJumping = true;
-			energy -= jumpCost;
-			GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower);
-		}
 	}
 
 	void FixedUpdate(){
-		Rigidbody rigidbody = GetComponent<Rigidbody>();
 		float moveX = Input.GetAxis("Horizontal");
 		float moveY = Input.GetAxis("Vertical");
+
+		HandleJump ();
 
 		if(canMove) {
 			Vector3	movement = new Vector3(moveX, 0.0f, moveY);
@@ -67,6 +61,36 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		transform.Rotate(Vector3.up, rotateSpeed * moveX * Time.deltaTime);
+	}
+	
+	void HandleJump(){
+		if(Input.GetKey(KeyCode.Space) && !isJumping && energy >= jumpCost && canMove){
+			rigidbody.useGravity = true;
+			canMove = false;
+			isJumping = true;
+			energy -= jumpCost;
+			rigidbody.AddRelativeForce(Vector3.up * jumpPower);
+			BurstLight();
+		}
+		if(isJumping && rigidbody.velocity.y <= 0){
+			Physics.gravity = Vector3.down * 1500.0f;
+		}
+	}
+	
+	void BurstLight(){
+		playerLight.range = maxSpotAngle * jumpLightMultiplier;
+	}
+	
+	void StayOnTheFloor(){
+		if(transform.position.y <= 1){
+			if(isJumping){
+				isJumping = false;
+				canMove = true;
+				rigidbody.useGravity = false;
+				Physics.gravity = Vector3.down * -9.81f;
+			}
+			transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
+		}
 	}
 
 	public void DisableMovement(){
@@ -82,15 +106,17 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	private void UpdateLighting(){
-		playerLight.intensity = Mathf.Clamp(
-			(energy / maxEnergy) * maxIntensity,
-			minIntensity,
-			maxIntensity
-		);
-		playerLight.range = Mathf.Clamp(
-			(energy / maxEnergy) * maxSpotAngle,
-			minSpotAngle,
-			maxSpotAngle
-		);
+		if(!isJumping){
+			playerLight.intensity = Mathf.Clamp(
+				(energy / maxEnergy) * maxIntensity,
+				minIntensity,
+				maxIntensity
+			);
+			playerLight.range = Mathf.Clamp(
+				(energy / maxEnergy) * maxSpotAngle,
+				minSpotAngle,
+				maxSpotAngle
+			);
+		}
 	}
 }
