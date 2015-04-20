0b2f1d91a5d110fb7e268fe891663f945e13f02b
Ben M <bmenesini@gmail.com>
2015-04-19 17:01:31 -0700

    Added firefly, animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,19 +15,27 @@ public class PlayerController : MonoBehaviour {
 	public float jumpCost = 30.0f;
 	public float jumpPower = 1500.0f;
 	public float jumpLightMultiplier = 1.5f;
+	public float animationSpeed = 0.25f;
 
 	public GameObject playerShot;
 	public Transform shotSpawn;
 	public Light playerLight;
-	public Camera mainCamera; 
+	public Camera mainCamera;
+	public GameObject wingsBillboard;
+	public GameObject bodyBillboard;
+	public Texture[] wingFrames;
+	public Texture[] bodyFrames;
 
 	private float nextFire;
+	private float nextAnimationTime;
 	private float energy;
 	private bool inWeb = false;
 	private bool isJumping = false;
 	private Rigidbody rbody;
 	private GameObject webSnare;
 	private Vector3 startPosition;
+	private int wingFrame = 0;
+	private int bodyFrame = 0;
 
 	void Start(){
 		energy = maxEnergy;
@@ -38,7 +46,21 @@ public class PlayerController : MonoBehaviour {
 	void Update(){
 		UpdateLighting ();
 		HandleFire();
+		UpdatePlayerBody();
 		StayOnTheFloor();
+	}
+
+	float EnergyPercent(){
+		return energy / maxEnergy;
+	}
+
+	void UpdatePlayerBody(){
+		int newFrame = (int)Mathf.Ceil(EnergyPercent() / 0.2f);
+		if(newFrame != bodyFrame){
+			bodyFrame = newFrame;
+			MeshRenderer renderer = bodyBillboard.GetComponent<MeshRenderer>();
+			renderer.material.mainTexture = bodyFrames[bodyFrame];
+		}
 	}
 
 	void HandleFire(){
@@ -61,7 +83,23 @@ public class PlayerController : MonoBehaviour {
 			rbody.velocity = Vector3.zero;
 		}
 
+		if(rbody.velocity != Vector3.zero){
+			if(Time.time >= nextAnimationTime){
+				Animate();
+				wingFrame++;
+				if(wingFrame >= wingFrames.Length){
+					wingFrame = 0;
+				}
+				nextAnimationTime = Time.time + animationSpeed;
+			}
+		}
+
 		transform.Rotate(Vector3.up, rotateSpeed * moveX * Time.deltaTime);
+	}
+
+	void Animate(){
+		MeshRenderer renderer = wingsBillboard.GetComponent<MeshRenderer>();
+		renderer.material.mainTexture = wingFrames[wingFrame];
 	}
 	
 	void HandleJump(){
